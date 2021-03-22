5fdbf2ec9e9e22391edb864dc267921bc9c377b6
vikrambenipal <vikrambenipal@gmail.com>
2021-03-22 16:18:40 -0400

    function for data fetch uses async and await now

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,13 +3,16 @@ import axios from 'axios';
 
 const List = () => {
 
+    let GetData = async (): Promise<void> => {
+        const data = await axios.get("https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=0&location=sf&full_time=on");
+        console.log(data);
+    }
+
     // Get Job Info from API
     useEffect(() => {
-        axios.get("https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=0&location=sf&full_time=on")
-        .then(res => {
-            console.log(res);
-        })
+        GetData();
     }, []);
+      
 
     return (
         <div>
